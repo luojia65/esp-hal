736844bc09e11c5aed2a1187efbe87c78253c306
Scott Mabin <scott@mabez.dev>
2022-09-05 13:45:42 +0100

    cargo +esp-dev run --example hello_world --release to repro

--- esp32-hal/examples/hello_world.rs
+++ esp32-hal/examples/hello_world.rs
@@ -4,7 +4,7 @@
 #![no_std]
 #![no_main]
 
-use core::fmt::Write;
+use core::{fmt::Write, time::Duration};
 
 use esp32_hal::{
     clock::ClockControl,
@@ -17,6 +17,8 @@ use esp32_hal::{
 use esp_backtrace as _;
 use nb::block;
 use xtensa_lx_rt::entry;
+
+use esp_println::{println, print};
 
 #[entry]
 fn main() -> ! {
@@ -35,9 +37,59 @@ fn main() -> ! {
     rtc.rwdt.disable();
 
     timer0.start(1u64.secs());
+    
+    println!("Align<u128>: {}", core::mem::align_of::<u128>());
+    println!("Size<u128>: {}", core::mem::size_of::<u128>());
+
+
+    println!("{}", u128::MAX);
+
+    test(); 
+
+    test2();
 
     loop {
         writeln!(serial0, "Hello world!").unwrap();
         block!(timer0.wait()).unwrap();
     }
+}
+
+
+#[inline(never)]
+fn test() {
+    let x = core::time::Duration::from_secs(10000);
+    let millis = x.as_millis();
+    println!("{millis}");
+}
+
+#[inline(never)]
+fn test2() {
+    let res = a(u64::MAX as u128, 12);
+    assert_eq!(res, 34028236692093846333424739891580135027);
+    println!("{res}");
+}
+
+
+#[inline(never)]
+pub fn print_u128(x: u128) {
+    for byte in x.to_be_bytes() {
+        print!("{:08b}", byte);
+    }
+    println!("");
+}
+
+#[inline(never)]
+fn a(i: u128, dummy: u32) -> u128 {
+    b(i) + dummy as u128
+}
+
+#[inline(never)]
+fn b(i: u128) -> u128 {
+    let (i, c) = c(i, 10);
+    i as u128 * c 
+}
+
+#[inline(never)]
+fn c(i: u128, d: u128) -> (u32, u128) {
+    ((i / d) as u32, i)
 }
